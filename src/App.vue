4a5f8285e56470ded717c714efcafd93e4ea82f8
ds asante <114578080+dsasante1@users.noreply.github.com>
2023-08-31 10:09:50 +0000

    fetch country flag

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+
+      <nav>
+        <span>Where in the world?</span>
+
+        <span class="toggleSection">
+          <img alt="toggle" class="toggle" src="@/assets/lightMoonToggle.svg" />
+
+          <span>Dark Mode</span>
+        
+        </span>
+        
+      </nav>
+
+      <div class="searchFilterSection">
+
+      </div>
+
+
+
+  <!-- <RouterView /> -->
+
+
+</template>
+
+<style scoped>
+
+</style>
